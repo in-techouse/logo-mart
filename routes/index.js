9b993a04285635cf4258e0afff7f86e0803ee506
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2019-11-16 10:04:36 +0500

    Enviormental Variables added

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,17 @@ var firebase= require('firebase');
 var router = express.Router();
 
 var firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
