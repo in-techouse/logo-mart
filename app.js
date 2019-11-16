f987ae17dba364e233e94136cb70dcbe9410aa94
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2019-11-16 09:05:39 +0500

    ProcFile Added

--- app.js
+++ app.js
@@ -38,5 +38,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 module.exports = app;
