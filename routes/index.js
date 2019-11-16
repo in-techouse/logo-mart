6726104fcdda4a585eb29f3265b94e792ce8c7da
hina kiran <hinakiran1996@gmail.com>
2019-11-16 09:28:52 +0500

    firebase added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,20 @@
 var express = require('express');
+var firebase= require('firebase');
 var router = express.Router();
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB-oh_jVJatT5kMBk2NYb7wZ15CRLQ7BDo",
+  authDomain: "logo-mart.firebaseapp.com",
+  databaseURL: "https://logo-mart.firebaseio.com",
+  projectId: "logo-mart",
+  storageBucket: "logo-mart.appspot.com",
+  messagingSenderId: "654640285591",
+  appId: "1:654640285591:web:9dceebf9d27f8ec999a6d2",
+  measurementId: "G-VPBLGZ4WQC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
