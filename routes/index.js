dd89a3c605fb8978e53b734b0f654f3012fdbd46
In-TecHouse <team.dev.man@gmail.com>
2019-11-16 09:36:50 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ var firebase= require('firebase');
 var router = express.Router();
 
 var firebaseConfig = {
-  apiKey: "AIzaSyB-oh_jVJatT5kMBk2NYb7wZ15CRLQ7BDo",
-  authDomain: "logo-mart.firebaseapp.com",
-  databaseURL: "https://logo-mart.firebaseio.com",
-  projectId: "logo-mart",
-  storageBucket: "logo-mart.appspot.com",
-  messagingSenderId: "654640285591",
-  appId: "1:654640285591:web:9dceebf9d27f8ec999a6d2",
-  measurementId: "G-VPBLGZ4WQC"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
