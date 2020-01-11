e2eea21ad49b1a3c5337a85f81f1d2fdffc7d4e9
hina kiran <hinakiran1996@gmail.com>
2020-01-11 10:00:19 +0500

    all paths of pages are set

--- routes/admin.js
+++ routes/admin.js
@@ -3,31 +3,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res) {
-    res.render("admins/index", { action: "index" });
-  });
-  router.get("/allLogos", function(req, res) {
-    res.render("admins/allLogos", { action: "index" });
-  });
-  router.get("/newLogos", function(req, res) {
-    res.render("admins/newLogos", { action: "index" });
-  });
-  router.get("/allMedicalCaps", function(req, res) {
-    res.render("admins/allMecalCaps", { action: "index" });
-  });
-  router.get("/newMedicalCaps", function(req, res) {
-    res.render("admins/newMedicalCaps", { action: "index" });
-  });
-  router.get("/allBusinessCard", function(req, res) {
-    res.render("admins/allBusinessCard", { action: "index" });
-  });
-  router.get("/newBusinessCard", function(req, res) {
-    res.render("admins/newBusinesscard", { action: "index" });
-  });
-  router.get("/allBrochures&Pamphlets", function(req, res) {
-    res.render("admins/newBrochures&Pamphlets", { action: "index" });
-  });
-  router.get("/allT_Shirts", function(req, res) {
-    res.render("admins/newT_Shirts", { action: "index" });
-  });
+  res.render("admins/index", { action: "index" });
+});
+router.get("/allLogos", function(req, res) {
+  res.render("admins/allLogos", { action: "index" });
+});
+router.get("/newLogo", function(req, res) {
+  res.render("admins/newLogos", { action: "index" });
+});
+router.get("/allMedicalCaps", function(req, res) {
+  res.render("admins/allMedicalCaps", { action: "index" });
+});
+router.get("/newMedicalCap", function(req, res) {
+  res.render("admins/newMedicalCaps", { action: "index" });
+});
+router.get("/allBusinessCards", function(req, res) {
+  res.render("admins/allBusinessCard", { action: "index" });
+});
+router.get("/newBusinessCard", function(req, res) {
+  res.render("admins/newBusinesscard", { action: "index" });
+});
+router.get("/allBrochuresAndPamphlets", function(req, res) {
+  res.render("admins/allBrochures&Pamphets", { action: "index" });
+});
+router.get("/newBrochureAndPamphlet", function(req, res) {
+  res.render("admins/newBrochures&Pamphlets", { action: "index" });
+});
+router.get("/allTShirts", function(req, res) {
+  res.render("admins/allT_Shirts", { action: "index" });
+});
+router.get("/newTShirt", function(req, res) {
+  res.render("admins/newT_Shirts", { action: "index" });
+});
 
-  module.exports = router;
+module.exports = router;
