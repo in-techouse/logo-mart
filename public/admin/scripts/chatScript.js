029f5490184073ae7b19cdbffb1b8cede5ffbc3d
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2020-05-12 07:18:19 +0500

    Chat on Admin side is 50$ done

--- public/admin/scripts/chatScript.js
+++ public/admin/scripts/chatScript.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   setTimeout(function () {
     $(".scrollbar-container").scrollTop($("#mainChatRoom")[0].scrollHeight);
   }, 1500);
+  loadPreviousChat();
   $("#messageBox").keypress(function (e) {
     if (e.which == 13) {
       let message = $("#messageBox").val();
@@ -10,34 +11,97 @@ $(document).ready(function () {
         return;
       }
 
-      console.log("Send Message: ", message);
+      let requestId = $("#requestId").val();
+      let userId = $("#userId").val();
+
+      let messageObj = {
+        id: "",
+        timeStamps: new Date().getTime(),
+        message: message,
+        userId: userId,
+      };
+      var formattedTime = moment(messageObj.timeStamps).format(
+        "HH:mm:ss a, dddd MMM YYYY"
+      );
+      console.log("messageObj: ", messageObj);
+      console.log("Formatted Time: ", formattedTime);
+
       let box = `<div class="row" style="margin-left: 20px; margin-right: 20px;"> 
-        <div class="float-right" style="float: right; width: 100%;">
-            <div class="chat-box-wrapper chat-box-wrapper-right">
-                <div style="float: right; width: 100%;">
-                    <div class="chat-box">
-                    <p style="width: 100%; text-align: right;">${message}</p>
-                    </div>
-                    <small class="opacity-6">
-                        <i class="fa fa-calendar-alt mr-1"></i>
-                        11:01 AM | Yesterday
-                    </small>
-                </div>
-            </div>
-        </div>
-    </div>`;
+          <div class="float-right" style="float: right; width: 100%;">
+              <div class="chat-box-wrapper chat-box-wrapper-right">
+                  <div style="float: right; width: 100%;">
+                      <div class="chat-box">
+                      <p style="width: 100%; text-align: right;">${messageObj.message}</p>
+                      </div>
+                      <small class="opacity-6">
+                          <i class="fa fa-calendar-alt mr-1"></i>
+                          ${formattedTime}
+                      </small>
+                  </div>
+              </div>
+          </div>
+      </div>`;
       $("#mainChatRoom").append(box);
       $("#messageBox").val("");
-      //   console.log("Height of Scroll Container: ", $(".scrollbar-container")[0]);
-      //   $(".scrollbar-container").scrollTop(
-      //     $(".scrollbar-container")[0].scrollHeight
-      //   );
 
       $(".scrollbar-container").scrollTop($("#mainChatRoom")[0].scrollHeight);
 
-      //   $(".scrollbar-container").scrollBottom(0);
+      let mId = firebase.database().ref().child("Chats").child(requestId).push()
+        .key;
+
+      messageObj.id = mId;
+      firebase
+        .database()
+        .ref()
+        .child("Chats")
+        .child(requestId)
+        .child(messageObj.id)
+        .set(messageObj);
 
       return false;
     }
   });
 });
+
+function loadPreviousChat() {
+  let requestId = $("#requestId").val();
+  let userId = $("#userId").val();
+  firebase
+    .database()
+    .ref()
+    .child("Chats")
+    .child(requestId)
+    .once("value")
+    .then((data) => {
+      data.forEach((d) => {
+        console.log("Single Message: ", d.val());
+        let m = d.val();
+        if (m.userId === userId) {
+          // Admin Message
+          var formattedTime = moment(m.timeStamps).format(
+            "HH:mm:ss a, dddd MMM YYYY"
+          );
+          let box = `<div class="row" style="margin-left: 20px; margin-right: 20px;"> 
+              <div class="float-right" style="float: right; width: 100%;">
+                  <div class="chat-box-wrapper chat-box-wrapper-right">
+                      <div style="float: right; width: 100%;">
+                          <div class="chat-box">
+                          <p style="width: 100%; text-align: right;">${m.message}</p>
+                          </div>
+                          <small class="opacity-6">
+                              <i class="fa fa-calendar-alt mr-1"></i>
+                              ${formattedTime}
+                          </small>
+                      </div>
+                  </div>
+              </div>
+          </div>`;
+          $("#mainChatRoom").append(box);
+        } else {
+          // User Message
+        }
+        $(".scrollbar-container").scrollTop($("#mainChatRoom")[0].scrollHeight);
+      });
+    })
+    .catch((err) => {});
+}
