948d85834f2e6411a39b5a0868d7be58094c72c7
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2020-08-16 17:23:15 +0500

    Send file in chat done

--- public/admin/scripts/chatScript.js
+++ public/admin/scripts/chatScript.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
     // console.log("Chat Document is ready");
     requestId = $("#requestId").val();
     userId = $("#userId").val();
-    currentUserId = $("#currentUserId").text();
-    // console.log("User Id: ", userId);
-    // console.log("Request Id: ", requestId);
-    // console.log("Current User Id: ", currentUserId.trim());
+    currentUserId = $("#currentUserId").text().trim();
+    console.log("User Id: ", userId);
+    console.log("Request Id: ", requestId);
+    console.log("Current User Id: ", currentUserId);
     setTimeout(function() {
         $(".scrollbar-container").scrollTop($("#mainChatRoom")[0].scrollHeight);
     }, 1500);
@@ -37,6 +37,8 @@ function sendAdminMessageToUser() {
         timeStamps: new Date().getTime(),
         message: message,
         userId: userId,
+        url: "",
+        fileName: "",
     };
     var formattedTime = moment(messageObj.timeStamps).format(
         "HH:mm:ss a, dddd MMM YYYY"
@@ -100,13 +102,19 @@ function loadPreviousChat() {
                 );
                 if (m.userId === userId) {
                     // Admin Message
+                    let mainMessage = ``;
+                    if (m.message === undefined || m.message.length < 1) {
+                        mainMessage = `<a href="${m.url}" target="_blank">${m.fileName}</a>`;
+                    } else {
+                        mainMessage = m.message;
+                    }
                     box = `
           <div class="row" style="margin-left: 20px; margin-right: 20px;"> 
               <div class="float-right" style="float: right; width: 100%;">
                   <div class="chat-box-wrapper chat-box-wrapper-right">
                       <div style="float: right; width: 100%;">
                           <div class="chat-box">
-                          <p style="width: 100%; text-align: right;">${m.message}</p>
+                          <p style="width: 100%; text-align: right;">${mainMessage}</p>
                           </div>
                           <small class="opacity-6">
                               <i class="fa fa-calendar-alt mr-1"></i>
@@ -117,13 +125,19 @@ function loadPreviousChat() {
               </div>
           </div>`;
                 } else {
+                    let mainMessage = ``;
+                    if (m.message === undefined || m.message.length < 1) {
+                        mainMessage = `<a href="${m.url}" target="_blank">${m.fileName}</a>`;
+                    } else {
+                        mainMessage = m.message;
+                    }
                     // User Message
                     box = `
           <div class="chat-wrapper p-1">
             <div class="chat-box-wrapper">
               <div>
                 <div class="chat-box">
-                  ${m.message}
+                  ${mainMessage}
                 </div>
                 <small class="opacity-6">
                   <i class="fa fa-calendar-alt mr-1"></i>
@@ -143,6 +157,110 @@ function loadPreviousChat() {
             listenToUserMessages();
         })
         .catch((err) => {});
+
+    $("#chatSelectFileAdmin").click(function() {
+        $("#chatFileAdmin").click();
+    });
+
+    $("#chatFileAdmin").change(function() {
+        selectedFiles = $("#chatFileAdmin").prop("files");
+        console.log("Files are: ", selectedFiles);
+        uploadAdminChatFile(selectedFiles[0]);
+    });
+}
+
+function uploadAdminChatFile(file) {
+    let dateTime = new Date().getTime();
+    var fileName = dateTime + file.name;
+    console.log("File: ", file);
+    console.log("Date Tme: ", dateTime);
+    console.log("File Name: ", fileName);
+
+    var ref = firebase.storage().ref("Chats").child("Order").child(requestId).child(fileName);
+    var uploadTask = ref.put(file);
+    console.log("File Upload Started");
+
+    uploadTask.on(
+        "state_changed",
+        function(snapshot) {
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            progress = progress.toFixed(2);
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                    console.log("Upload is paused");
+                    break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                    console.log("Upload is running");
+                    break;
+            }
+        },
+        function(error) {
+            $("#submit").prop("disabled", false);
+            console.log("Upload Error: ", error);
+        },
+        function() {
+            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                console.log("File available at", downloadURL);
+                sendAdminFileToUser(downloadURL, file.name);
+            });
+        }
+    );
+}
+
+function sendAdminFileToUser(url, fileName) {
+    let messageObj = {
+        id: "",
+        timeStamps: new Date().getTime(),
+        message: "",
+        userId: userId,
+        url: url,
+        fileName: fileName,
+    };
+
+    var formattedTime = moment(messageObj.timeStamps).format(
+        "HH:mm:ss a, dddd MMM YYYY"
+    );
+    // console.log("messageObj: ", messageObj);
+    // console.log("Formatted Time: ", formattedTime);
+
+    let box = `<div class="row" style="margin-left: 20px; margin-right: 20px;"> 
+    <div class="float-right" style="float: right; width: 100%;">
+        <div class="chat-box-wrapper chat-box-wrapper-right">
+            <div style="float: right; width: 100%;">
+                <div class="chat-box">
+                <p style="width: 100%; text-align: right;">
+                    <a href="${messageObj.url}" target="_blank">${messageObj.fileName}</a>
+                </p>
+                </div>
+                <small class="opacity-6">
+                    <i class="fa fa-calendar-alt mr-1"></i>
+                    ${formattedTime}
+                </small>
+            </div>
+        </div>
+    </div>
+    </div>`;
+    $("#mainChatRoom").append(box);
+    $("#messageBox").val("");
+
+    $(".scrollbar-container").animate({
+            scrollTop: $("#mainChatRoom").height(),
+        },
+        1000
+    );
+
+    let mId = firebase.database().ref().child("Chats").child(requestId).push()
+        .key;
+
+    messageObj.id = mId;
+    firebase
+        .database()
+        .ref()
+        .child("Chats")
+        .child(requestId)
+        .child(messageObj.id)
+        .set(messageObj);
 }
 
 function listenToUserMessages() {
@@ -154,7 +272,7 @@ function listenToUserMessages() {
         .child("Chats")
         .child(requestId)
         .orderByChild("userId")
-        .equalTo(currentUserId.trim());
+        .equalTo(currentUserId);
     messageRef.on("value", function(snapshot) {
         // console.log("New Message Received");
         const maxTimeStamps = Math.max.apply(
@@ -183,12 +301,18 @@ function setUserMessage(m) {
     const formattedTime = moment(m.timeStamps).format(
         "HH:mm:ss a, dddd MMM YYYY"
     );
+    let mainMessage = ``;
+    if (m.message === undefined || m.message.length < 1) {
+        mainMessage = `<a href="${m.url}" target="_blank">${m.fileName}</a>`;
+    } else {
+        mainMessage = m.message;
+    }
     const box = `
           <div class="chat-wrapper p-1">
             <div class="chat-box-wrapper">
               <div>
                 <div class="chat-box">
-                  ${m.message}
+                  ${mainMessage}
                 </div>
                 <small class="opacity-6">
                   <i class="fa fa-calendar-alt mr-1"></i>
--- public/javascripts/userChat.js
+++ public/javascripts/userChat.js
@@ -1,4 +1,5 @@
 const messages = [];
+let currentChattingUserEmail = "";
 $(document).ready(function() {
     // console.log("Chat page is ready");
     let rId = localStorage.getItem("rId");
@@ -31,13 +32,114 @@ $(document).ready(function() {
 
             $("#userMainChatRoom").append(firstMessage);
             $("#userOrderDetail").append(firstMessage);
+            currentChattingUserEmail = data.val().email;
             loadPreviousChat(rId, data.val().email);
             enableUserMessaging(rId, data.val().email);
         })
         .catch((err) => {
             // console.log("Error: ", err);
         });
+
+    $("#chatSelectFileUser").click(function() {
+        $("#chatFileUser").click();
+    });
+
+    $("#chatFileUser").change(function() {
+        selectedFiles = $("#chatFileUser").prop("files");
+        console.log("Files are: ", selectedFiles);
+        uploadUserChatFile(selectedFiles[0]);
+    });
 });
+
+function uploadUserChatFile(file) {
+    let rId = localStorage.getItem("rId");
+    let dateTime = new Date().getTime();
+    var fileName = dateTime + file.name;
+    console.log("File: ", file);
+    console.log("Date Tme: ", dateTime);
+    console.log("File Name: ", fileName);
+
+    var ref = firebase.storage().ref("Chats").child("Order").child(rId).child(fileName);
+    var uploadTask = ref.put(file);
+    console.log("File Upload Started");
+
+    uploadTask.on(
+        "state_changed",
+        function(snapshot) {
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            progress = progress.toFixed(2);
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                    console.log("Upload is paused");
+                    break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                    console.log("Upload is running");
+                    break;
+            }
+        },
+        function(error) {
+            $("#submit").prop("disabled", false);
+            console.log("Upload Error: ", error);
+        },
+        function() {
+            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                console.log("File available at", downloadURL);
+                sendUserFileToAdmin(rId, downloadURL, file.name)
+            });
+        }
+    );
+}
+
+
+function sendUserFileToAdmin(rId, url, fileName) {
+    let messageObj = {
+        id: "",
+        timeStamps: new Date().getTime(),
+        message: "",
+        userId: currentChattingUserEmail,
+        url: url,
+        fileName: fileName,
+    };
+
+    // console.log("Message Obj is: ", messageObj);
+
+    var formattedTime = moment(messageObj.timeStamps).format(
+        "HH:mm:ss a, dddd MMM YYYY"
+    );
+
+    let messageDiv = `
+    <div class="row msg" style="margin-left: 20px; margin-right: 20px;">
+        <div class="float-right" style="float: right; width: 100%;">
+            <div class="chat-box-wrapper chat-box-wrapper-right">
+                <div style="float: right; width: 100%;">
+                    <div class="chat-box">                        
+                        <p style="width: 100%; text-align: right; color: #303030;">
+                            <a href="${messageObj.url}" target="_blank">${messageObj.fileName}</a>
+                        </p>
+                    </div>
+                    <small class="opacity-6">
+                        <i class="fa fa-calendar-alt mr-1"></i>
+                        ${formattedTime}
+                    </small>
+                </div>
+            </div>
+        </div>
+    </div>`;
+
+    $("#userMainChatRoom").append(messageDiv);
+    $("#userMessageBox").val("");
+    let mId = firebase.database().ref().child("Chats").child(rId).push().key;
+    messageObj.id = mId;
+    firebase.database().ref().child("Chats").child(rId).child(mId).set(messageObj);
+    // console.log("Message Obj Saved to database");
+    $(".scrollbar-container").animate({
+            scrollTop: $("#userMainChatRoom").height(),
+        },
+        1000
+    );
+    // console.log("Scroll to bottom");
+}
 
 function enableUserMessaging(rId, email) {
     $("#userSendMessage").click(function() {
@@ -62,6 +164,8 @@ function senduserMessageToAdmin(rId, email) {
         timeStamps: new Date().getTime(),
         message: message,
         userId: email,
+        url: "",
+        fileName: ""
     };
 
     var formattedTime = moment(messageObj.timeStamps).format(
@@ -113,13 +217,19 @@ function loadPreviousChat(rId, email) {
                     "HH:mm:ss a, dddd MMM YYYY"
                 );
                 if (message.userId === email) {
+                    let mainMessage = ``;
+                    if (message.message === undefined || message.message.length < 1) {
+                        mainMessage = `<a href="${message.url}" target="_blank">${message.fileName}</a>`;
+                    } else {
+                        mainMessage = message.message;
+                    }
                     messageDiv = `
             <div class="row msg" style="margin-left: 20px; margin-right: 20px;">
                 <div class="float-right" style="float: right; width: 100%;">
                     <div class="chat-box-wrapper chat-box-wrapper-right">
                         <div style="float: right; width: 100%;">
                             <div class="chat-box">
-                                <p style="width: 100%; text-align: right; color: #303030;">${message.message}</p>
+                                <p style="width: 100%; text-align: right; color: #303030;">${mainMessage}</p>
                             </div>
                             <small class="opacity-6">
                                 <i class="fa fa-calendar-alt mr-1"></i>
@@ -130,12 +240,18 @@ function loadPreviousChat(rId, email) {
                 </div>
             </div>`;
                 } else {
+                    let mainMessage = ``;
+                    if (message.message === undefined || message.message.length < 1) {
+                        mainMessage = `<a href="${message.url}" target="_blank">${message.fileName}</a>`;
+                    } else {
+                        mainMessage = message.message;
+                    }
                     messageDiv = `
             <div class="chat-wrapper p-1 msg">
                 <div class="chat-box-wrapper">
                     <div>
                         <div class="chat-box">
-                            <p style="width: 100%; text-align: left; color: #303030;">${message.message}</p>
+                            <p style="width: 100%; text-align: left; color: #303030;">${mainMessage}</p>
                         </div>
                         <small class="opacity-6">
                             <i class="fa fa-calendar-alt mr-1"></i>
@@ -194,12 +310,18 @@ function setAdminMessage(message) {
     const formattedTime = moment(message.timeStamps).format(
         "HH:mm:ss a, dddd MMM YYYY"
     );
+    let mainMessage = ``;
+    if (message.message === undefined || message.message.length < 1) {
+        mainMessage = `<a href="${message.url}" target="_blank">${message.fileName}</a>`;
+    } else {
+        mainMessage = message.message;
+    }
     const messageDiv = `
     <div class="chat-wrapper p-1 msg">
         <div class="chat-box-wrapper">
             <div>
                 <div class="chat-box">
-                    <p style="width: 100%; text-align: left; color: #303030;">${message.message}</p>
+                    <p style="width: 100%; text-align: left; color: #303030;">${mainMessage}</p>
                 </div>
                 <small class="opacity-6">
                     <i class="fa fa-calendar-alt mr-1"></i>
